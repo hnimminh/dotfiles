8bdc4ee149ac6fb0e65eb084cdc7e90e08db0f01
Minh Minh <hnimminh@outlook.com>
2020-04-29 18:00:28 +0700

    append install step

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 # tmux
 apt-get install tmux
 cp tmux.conf ~/.tmux.conf
+
+# powerline
+pip3 install powerline-status
+~/.local/lib/python3.8/site-packages/powerline
+
+#/home/minh/.local/lib/python3.8/site-packages/powerline/bindings/tmux/powerline.conf
+sudo apt-get install fonts-powerline
