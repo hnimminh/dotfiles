f64e7cfed96b0a8f876a5a824fe0d2ac4410a444
Minh Minh <hnimminh@outlook.com>
2020-05-02 13:34:53 +0700

    fish setting up

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,21 +1,20 @@
-# FONT: USE NERD ONLY
+# USE NERD FONT ONLY
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
 # COLOR STYLE
 set -g theme_color_scheme dracula
 # SHOW CURRENT USER
 set -g default_user your_normal_user
-# DATETIME DISPLAY
+# DATETIME
 set -g theme_display_date yes
 set -g theme_date_format "+ %H:%M:%S %Z %d-%m" 
 set -g theme_date_timezone Asia/Ho_Chi_Minh
-# GIT DISPLAY 
+# GIT SETTING 
 set -g theme_display_git yes
 set -g theme_use_abbreviated_branch_name yes
 #
 #
 ####### VERBAL OPTIMIZATION USE
-#
 #
 set -g theme_title_use_abbreviated_path yes
 set -g theme_display_ruby no
